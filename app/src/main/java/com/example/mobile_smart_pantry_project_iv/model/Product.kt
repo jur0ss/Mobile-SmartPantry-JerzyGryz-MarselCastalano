2e03d87f3cd5f15017832f08ba1b13176c97abe7
jurekg <jurekgz27@gmail.com>
2026-02-23 13:34:17 +0100

    Stworzono klasę kotlinową Product

---
+++ app/src/main/java/com/example/mobile_smart_pantry_project_iv/model/Product.kt
@@ -0,0 +1,14 @@
+package com.example.mobile_smart_pantry_project_iv.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Product (
+    val uuid: String,
+    val name: String,
+    val quantity: Int,
+    val category: String,
+    val imageref: String
+
+
+): java.io.Serializable
